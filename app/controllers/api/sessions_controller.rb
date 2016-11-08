a4039bffd0f8374e1409c926a269ceef935628c0
Nam Kim <nryulkim@gmail.com>
2016-11-07 19:58:09 -0500

    Setup authentication

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,17 @@
 class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_creds(params[:user][:username], params[:user][:password])
+    if @user
+      login(@user)
+      render :create
+    else
+      @errors = ["Please check your username and password combination"]
+      render json: @errors, status: 422
+    end
+  end
+
+  def destroy
+    logout
+    render json: {}
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,48 @@
 #
 
 class User < ApplicationRecord
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  before_validation :ensure_token
+
+  def self.find_by_creds(username, password)
+    @user = User.find_by(username: username)
+    if @user
+      if @user.is_password?(password)
+        return @user
+      else
+        return nil
+      end
+    end
+  end
+
+  attr_reader :password
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password;
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_token
+    self.session_token = gen_token
+    self.save
+    self.session_token
+  end
+
+  private
+
+  def ensure_token
+    self.session_token ||= gen_token
+  end
+
+  def gen_token
+    SecureRandom.urlsafe_base64
+  end
+
 end
