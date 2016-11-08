53ba2902cb2735df1b7a8888066062e6be1e40c5
Nam Kim <nryulkim@gmail.com>
2016-11-07 20:47:59 -0500

    Setup header

---
+++ frontend/components/header/header.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router';
+
+class Header extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  signout(){
+    this.props.logout();
+    this.props.router.replace("#");
+  }
+
+  getButtons(){
+    const { currentUser } = this.props;
+    if(currentUser){
+      return [
+        <h3>{currentUser.username}</h3>,
+        <Link className="button" to="/logout">Sign Out</Link>
+      ];
+    }else{
+      return [
+        <Link className="button" to="/login">Sign In</Link>,
+        <Link className="button" to="/signup">Sign Up</Link>
+      ];
+    }
+  }
+
+  render(){
+    const { currentUser } = this.props;
+    let button1, button2;
+    [button1, button2] = this.getButtons();
+
+    return(
+      <div className="head">
+        {button1}
+        {button2}
+      </div>
+    );
+  }
+}
+
+export default withRouter(Header);
