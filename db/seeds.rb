9edced23ee69e916536a73d3368452e316f620a8
Nam Kim <nryulkim@gmail.com>
2016-11-09 23:49:38 -0500

    Seed destroys database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Location.destroy_all
+UserLocation.destroy_all
+User.destroy_all
